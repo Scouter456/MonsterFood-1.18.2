1243f834c865427244b768985294e7c52371128e
Scouter456 <markveenstra14@gmail.com>
2022-06-23 22:13:33 +0200

    Changed item

--- src/main/java/com/scouter/monsterfood/items/MFItems.java
+++ src/main/java/com/scouter/monsterfood/items/MFItems.java
@@ -23,14 +23,14 @@ public class MFItems {
     public static final RegistryObject<Item> bubble_lilly = ITEMS.register("bubble_lilly", () -> new Item(Registration.defaultBuilder()));
 
     //Knives
-    public static final RegistryObject<Item> WOOD_KNIFE = ITEMS.register("wood_knife", () -> new KnifeItem(Tiers.WOOD ,Registration.defaultBuilder().defaultDurability(Tiers.WOOD.getUses()).stacksTo(1)));
-    public static final RegistryObject<Item> STONE_KNIFE = ITEMS.register("stone_knife", () -> new KnifeItem(Tiers.STONE ,Registration.defaultBuilder().defaultDurability(Tiers.STONE.getUses()).stacksTo(1)));
-    public static final RegistryObject<Item> IRON_KNIFE = ITEMS.register("iron_knife", () -> new KnifeItem(Tiers.IRON ,Registration.defaultBuilder().defaultDurability(Tiers.IRON.getUses()).stacksTo(1)));
-    public static final RegistryObject<Item> GOLDEN_KNIFE = ITEMS.register("golden_knife", () -> new KnifeItem(Tiers.GOLD ,Registration.defaultBuilder().defaultDurability(Tiers.GOLD.getUses()).stacksTo(1)));
-    public static final RegistryObject<Item> DIAMOND_KNIFE = ITEMS.register("diamond_knife", () -> new KnifeItem(Tiers.DIAMOND ,Registration.defaultBuilder().defaultDurability(Tiers.DIAMOND.getUses()).stacksTo(1)));
-    public static final RegistryObject<Item> NETHERITE_KNIFE = ITEMS.register("netherite_knife", () -> new KnifeItem(Tiers.NETHERITE ,Registration.defaultBuilder().fireResistant().defaultDurability(Tiers.NETHERITE.getUses()).stacksTo(1)));
-    public static final RegistryObject<Item> ADAMANTITE_KNIFE = ITEMS.register("adamantite_knife", () -> new KnifeItem(/**Temporary**/Tiers.NETHERITE ,Registration.defaultBuilder().fireResistant().defaultDurability(/*TODO add adamantite tier*/4000).stacksTo(1)));
-    public static final RegistryObject<Item> MITHRIL_KNIFE = ITEMS.register("mithril_knife", () -> new KnifeItem(/**Temporary**/Tiers.NETHERITE ,Registration.defaultBuilder().fireResistant().defaultDurability(/*TODO add mithril tier*/5000).stacksTo(1)));
+    public static final RegistryObject<Item> WOOD_KNIFE = ITEMS.register("wood_knife", () -> new KnifeItem(Tiers.WOOD ,Registration.defaultBuilder().defaultDurability(Tiers.WOOD.getUses())));
+    public static final RegistryObject<Item> STONE_KNIFE = ITEMS.register("stone_knife", () -> new KnifeItem(Tiers.STONE ,Registration.defaultBuilder().defaultDurability(Tiers.STONE.getUses())));
+    public static final RegistryObject<Item> IRON_KNIFE = ITEMS.register("iron_knife", () -> new KnifeItem(Tiers.IRON ,Registration.defaultBuilder().defaultDurability(Tiers.IRON.getUses())));
+    public static final RegistryObject<Item> GOLDEN_KNIFE = ITEMS.register("golden_knife", () -> new KnifeItem(Tiers.GOLD ,Registration.defaultBuilder().defaultDurability(Tiers.GOLD.getUses())));
+    public static final RegistryObject<Item> DIAMOND_KNIFE = ITEMS.register("diamond_knife", () -> new KnifeItem(Tiers.DIAMOND ,Registration.defaultBuilder().defaultDurability(Tiers.DIAMOND.getUses())));
+    public static final RegistryObject<Item> NETHERITE_KNIFE = ITEMS.register("netherite_knife", () -> new KnifeItem(Tiers.NETHERITE ,Registration.defaultBuilder().fireResistant().defaultDurability(Tiers.NETHERITE.getUses())));
+    public static final RegistryObject<Item> ADAMANTITE_KNIFE = ITEMS.register("adamantite_knife", () -> new KnifeItem(/**Temporary**/Tiers.NETHERITE ,Registration.defaultBuilder().fireResistant().defaultDurability(/*TODO add adamantite tier*/4000)));
+    public static final RegistryObject<Item> MITHRIL_KNIFE = ITEMS.register("mithril_knife", () -> new KnifeItem(/**Temporary**/Tiers.NETHERITE ,Registration.defaultBuilder().fireResistant().defaultDurability(/*TODO add mithril tier*/5000)));
 
 
     //Foods
